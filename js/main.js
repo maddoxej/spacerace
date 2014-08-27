0a8b51ee174fdc44ddd7ca614c6187833fa07462
maddoxej <thaw-koding@usa.net>
2014-08-27 02:58:27 +0000

    moved JS from SpaceRace.html to main.js for browserify

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var $ = require('jquery');
+require('jquery-ui')
+$('#buildDialog').dialog({
+    position: {
+        at: "bottom left"
+    },
+    buttons: {
+        Ok: function() {
+            $(this).dialog("close"); //closing on Ok click
+        }
+    }
+});
+var SpaceRace = require('./SpaceRace').SpaceRace;
+var Board = require('./Board').Board;
+var spaceRace = new SpaceRace();
+spaceRace.createLand();
+spaceRace.showBoard(new Board());
+spaceRace.createResources();
